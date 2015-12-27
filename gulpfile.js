e98b2dd66f9ee629f0c739514665dec97a3880cf
Micah Koki <micah.koki@gmail.com>
2015-12-26 17:02:44 -0800

    adds gulp-sass task to seed to help compile sass/scss to css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 
 var babel = require('gulp-babel');
 
+var sass = require('gulp-sass');
+
+
+//es6 task, brah
 gulp.task('default', function() {
   return gulp.src('src/test.js')
     .pipe(babel({
@@ -9,9 +13,11 @@ gulp.task('default', function() {
     }))
     .pipe(gulp.dest('dist'));
 });
-//es6 task, brah
-
-
 
 
 //sass task
+gulp.task('sass', function () {
+  gulp.src('scss/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/css'));
+});
